1170a52918a49c0e2cddb534643b187d61e980cf
pgrho <shipwreck@live.jp>
2020-11-11 10:25:54 +0900

    .NET 5.0

---
+++ src/Shipwreck.BlazorJqueryToast.Demo/gulpfile.js
@@ -0,0 +1,12 @@
+﻿/// <binding AfterBuild='default' Clean='clean' />
+var gulp = require("gulp");
+var del = require('del');
+
+gulp.task('clean', function () {
+    return del(['wwwroot/js', 'wwwroot/css']);
+});
+gulp.task('lib', function () {
+    return gulp.src(['../Shipwreck.BlazorJqueryToast/wwwroot/**/*'])
+        .pipe(gulp.dest('wwwroot'));
+});
+gulp.task('default', gulp.series(['clean', 'lib']));
